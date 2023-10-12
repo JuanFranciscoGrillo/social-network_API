21ef7a24390b2fba26187cf1a2d729cf62a057c2
Juan Francisco Grillo <jfrgm1986@gmail.com>
2023-10-12 13:43:58 -0400

    Added comments to the main server file for clarity and code documentation

--- server.js
+++ server.js
@@ -1,16 +1,27 @@
+// Import necessary dependencies and modules
 const express = require('express');
 const routes = require('./routes');
 const db = require('./config/connection');
 
+// Create an instance of the Express application
 const app = express();
+
+// Set the port for the server
 const PORT = process.env.PORT || 3001;
 
+// Middleware to parse JSON request bodies
 app.use(express.json());
+
+// Middleware to parse URL-encoded data
 app.use(express.urlencoded({ extended: true }));
+
+// Use the routes defined in the routes directory
 app.use(routes);
 
+// Connect to the database and start the server
+// Once the database connection is open, start the server
 db.once('open', () => {
     app.listen(PORT, () => {
-        console.log(`App running on port ${PORT}!`);
+        console.log(`App running on port ${PORT}!`); // Log the port where the app is running
     });
 });
