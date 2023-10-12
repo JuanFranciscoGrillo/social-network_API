1858f4f4675b3c64f994c7beb666c0071bf31b2b
Juan Francisco Grillo <jfrgm1986@gmail.com>
2023-10-12 13:41:30 -0400

    Added comments to the dateFormat utility function for better understanding

--- utils/dateFormat.js
+++ utils/dateFormat.js
@@ -1,5 +1,8 @@
+// Define a function called dateFormat
+// This function takes a date as a parameter and returns it as a localized string
 const dateFormat = (date) => {
-    return date.toLocaleString();
+    return date.toLocaleString(); // Convert the date to a string using the local date and time representation
 };
 
+// Export the dateFormat function to be used in other parts of the application
 module.exports = dateFormat;
