e28c0371db0947b285214ccdd9378446a18c4d94
Juan Francisco Grillo <jfrgm1986@gmail.com>
2023-10-12 13:35:48 -0400

    Annotated the thought routes for better clarity

--- routes/api/thoughtroutes.js
+++ routes/api/thoughtroutes.js
@@ -1,4 +1,5 @@
-const router = require('express').Router();
+// Import necessary modules
+const router = require('express').Router(); // Importing Router from express
 const {
     getAllThoughts,
     getThoughtById,
@@ -7,28 +8,29 @@ const {
     deleteThought,
     addReaction,
     deleteReaction
-} = require('../../controllers/thoughtcontroller');
+} = require('../../controllers/thoughtcontroller'); // Destructuring methods from the thoughtcontroller
 
 // Routes for /api/thoughts
 router
     .route('/')
-    .get(getAllThoughts)
-    .post(createThought);
+    .get(getAllThoughts) // Route to get all thoughts
+    .post(createThought); // Route to create a new thought
 
 // Routes for /api/thoughts/:id
 router
     .route('/:id')
-    .get(getThoughtById)
-    .put(updateThought)
-    .delete(deleteThought);
+    .get(getThoughtById) // Route to get a single thought by ID
+    .put(updateThought) // Route to update a thought by ID
+    .delete(deleteThought); // Route to delete a thought by ID
 
 // Routes for reactions
 router
     .route('/:thoughtId/reactions')
-    .post(addReaction);
+    .post(addReaction); // Route to add a reaction to a thought
 
 router
     .route('/:thoughtId/reactions/:reactionId')
-    .delete(deleteReaction);
+    .delete(deleteReaction); // Route to delete a reaction from a thought by ID
 
+// Exporting the configured router to be used in other parts of the application
 module.exports = router;
