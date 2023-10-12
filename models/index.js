5a1ad56ec080bb32291c7855964f88af5414d53c
Juan Francisco Grillo <jfrgm1986@gmail.com>
2023-10-12 13:30:03 -0400

    Added comments to the models index for clarity

--- models/index.js
+++ models/index.js
@@ -1,4 +1,8 @@
+// Importing User model
 const User = require('./User');
+
+// Importing Thought model
 const Thought = require('./Thought');
 
+// Exporting the imported models to be used in other parts of the application
 module.exports = { User, Thought };
