83f09870595bac609f6e25dfc90565fd88aa2466
Juan Francisco Grillo <jfrgm1986@gmail.com>
2023-10-12 13:38:02 -0400

    Added comments to main router file for clarity and understanding

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
+// Import the express Router
 const router = require('express').Router();
+
+// Import the API routes
 const apiRoutes = require('./api');
 
+// Use the API routes when the path starts with '/api'
 router.use('/api', apiRoutes);
 
+// Export the router for use in other parts of the application
 module.exports = router;
