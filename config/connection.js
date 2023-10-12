32bb81c8fd166b61d7fb110d382291cf0c706596
Juan Francisco Grillo <jfrgm1986@gmail.com>
2023-10-12 13:24:08 -0400

    Simplified comments in mongoose configuration file

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose');
 
+// Connecting to MongoDB
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api', {
-    useFindAndModify: false,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+    useFindAndModify: false, // Using the latest findAndModify function
+    useNewUrlParser: true, // Using the new URL parser
+    useUnifiedTopology: true // Using unified topology
 });
 
+// Setting useCreateIndex to true
 mongoose.set('useCreateIndex', true);
 
+// Exporting the connection
 module.exports = mongoose.connection;
