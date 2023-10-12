b397ba0e5f64a1e0c9c064eaea06703a231f7591
Juan Francisco Grillo <jfrgm1986@gmail.com>
2023-10-12 13:47:48 -0400

    Added dotenv for secure environment variable management

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+// Load environment variables from .env file
+require('dotenv').config();
+
 // Import necessary dependencies and modules
 const express = require('express');
 const routes = require('./routes');
