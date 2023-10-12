a5d87c949064646e5744c7d27b23972dfe1a212f
Juan Francisco Grillo <jfrgm1986@gmail.com>
2023-10-12 13:34:49 -0400

    Added comments to router configuration for clarity

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,13 @@
-const router = require('express').Router();
-const thoughtRoutes = require('./thoughtroutes');
-const userRoutes = require('./userroutes');
+// Import necessary modules
+const router = require('express').Router(); // Importing Router from express
+const thoughtRoutes = require('./thoughtroutes'); // Importing routes for thoughts
+const userRoutes = require('./userroutes'); // Importing routes for users
 
-router.use('/thoughts', thoughtRoutes);
+// Using thoughtRoutes when '/thoughts' endpoint is hit
+router.use('/thoughts', thoughtRoutes); 
+
+// Using userRoutes when '/users' endpoint is hit
 router.use('/users', userRoutes);
 
+// Exporting the configured router to be used in other parts of the application
 module.exports = router;
